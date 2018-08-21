3edb62438d891bdf970e14d229528705de1542fc
DominicLourdusamy <dominic.lourdusamy@rntbci.com>
2018-08-21 14:26:19 +0530

    Java file and Pom file is pushed initially

---
+++ Maven.Cucumber.Sample/src/test/java/Common/LoginPage.java
@@ -0,0 +1,14 @@
+package Common;
+
+import org.testng.annotations.Test;
+
+public class LoginPage {
+	
+@Test
+		
+		public void loadPage() throws InterruptedException
+		{
+		System.out.println("Login Successfully");
+		}
+	}
+
